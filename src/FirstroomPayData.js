415ba5ec0b10194ad00e1f0a69fba046337adb1c
Rahul Bharodiya <rbkumar01234@gmail.com>
2023-04-12 13:33:40 +0530

    some changes

--- src/FirstroomPayData.js
+++ src/FirstroomPayData.js
@@ -15,6 +15,8 @@ import Logo from "./Logo";
 
 import { Helmet } from "react-helmet";
 
+import Deleteimg from "./image/delete.jpg";
+
 const FirstroomPayData = () => {
   const [Firstroomalldata, setFirstRoomAllData] = useState([]);
   const [defaultvalue, setDefaultValue] = useState("");
@@ -87,7 +89,7 @@ const FirstroomPayData = () => {
               justifyContent: "center",
             }}
           >
-            7star Room Total Pay Data
+            5star Room Total Pay Data
           </p>
           <p
             style={{
@@ -171,32 +173,56 @@ const FirstroomPayData = () => {
         backdrop="static"
         keyboard={false}
       >
-        <Modal.Header closeButton>
-          <Modal.Title>final delete this data</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-1" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                defaultValue={defaultvalue.email}
-                disabled
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button
-            variant="primary"
-            onClick={(_id) => delete_first_room_user_data(defaultvalue._id)}
-          >
-            Delete
-          </Button>
-        </Modal.Footer>
+        <div style={{ backgroundColor: "rgb(162, 179, 139)" }}>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm delete this data</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form
+              style={{
+                boxShadow: "5px 5px 10px black ",
+                backgroundColor: "rgb(215, 233, 185)",
+                padding: "10px",
+              }}
+            >
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <img
+                  src={Deleteimg}
+                  alt="wrong"
+                  style={{
+                    height: "80px",
+                  }}
+                />
+              </div>
+              <hr></hr>
+              <Form.Group className="mb-1" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  defaultValue={defaultvalue.email}
+                  disabled
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button
+              variant="primary"
+              onClick={(_id) => delete_first_room_user_data(defaultvalue._id)}
+            >
+              Delete
+            </Button>
+          </Modal.Footer>
+        </div>
       </Modal>
     </div>
   );
